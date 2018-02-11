b28417cadda66d213d5a3b3a170f5152279d345f
jecshcier <alsk123111@icloud.com>
2018-02-11 11:31:35 +0800

    update

--- dev/index.js
+++ dev/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Tree, Button, Radio, Icon, Modal} from 'antd';
+import {Tree, Button, Radio, Icon, Modal,Input} from 'antd';
 import 'antd/dist/antd.css'
 
 const TreeNode = Tree.TreeNode;
@@ -65,20 +65,6 @@ let CHAPTER = [{
     }]
   }]
 
-class AddMenu extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  
-  
-  render() {
-    return <div>
-      <input type="text" rel={this.props.type}/>
-      <button>确定</button>
-      <button onClick={this.props.cancel}>取消</button>
-    </div>
-  }
-}
 
 class List extends React.Component {
   constructor(props) {
@@ -145,10 +131,11 @@ class BookConfigView extends React.Component {
 class MenuEdit extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props)
-    this.newChapter = this.newChapter.bind(this)
-    this.cancelAddView = this.cancelAddView.bind(this)
-    this.onCheck = this.onCheck.bind(this)
+    this.onSelect = this.onSelect.bind(this)
+    this.add = this.add.bind(this)
+    this.modalOK = this.modalOK.bind(this)
+    this.modalCancel = this.modalCancel.bind(this)
+    this.modalDom = null
     this.addView = null
     
     let chapterArr = []
@@ -179,42 +166,49 @@ class MenuEdit extends React.Component {
       }
     }
     this.state = {
-      checkedKeys: [],
-      chapterData: chapterArr
+      selNode: "",
+      chapterData: chapterArr,
+      showModal: false
     }
     // this.list = chapterArr
   }
   
-  newChapter(e) {
-    if (this.state.addState) {
-      return
+  onSelect(selectKeys, e) {
+    this.setState({selNode: selectKeys.length ? selectKeys[0] : ""})
+    console.log(selectKeys)
+    console.log(e)
+  }
+  
+  modalOK() {
+  
+  }
+  
+  modalCancel() {
+    this.setState({showModal: false})
+  }
+  
+  add() {
+    if (this.addType === null) {
+      console.log('不可操作')
+      return;
     }
-    this.setState({})
-    this.addView = <AddMenu type={'chapter'} cancel={this.cancelAddView}/>
-  }
-  
-  newSection(e) {
-    if (this.state.addState) {
-      return
+    switch (this.addType) {
+      case 0:
+        console.log("新增章")
+        this.modalDom = <Input placeholder="章名称" />
+        break
+      case 1:
+        console.log("新增节")
+        this.modalDom = <Input placeholder="节名称" />
+        break
+      case 2:
+        console.log("新增课")
+        this.modalDom = <Input placeholder="课名称" />
+        break
+      default:
+        break
     }
-    this.setState({})
-    this.addView = <AddMenu type={'section'} cancel={this.cancelAddView}/>
-  }
-  
-  newCourse(e) {
-    if (this.state.addState) {
-      return
-    }
-    this.setState({})
-    this.addView = <AddMenu type={'course'} cancel={this.cancelAddView}/>
-  }
-  
-  cancelAddView() {
-    this.addView = null
-  }
-  
-  onCheck(checkedKeys) {
-    this.setState({checkedKeys: checkedKeys.checked})
+    this.setState({showModal: true})
   }
   
   renderTreeNodes(data) {
@@ -231,25 +225,53 @@ class MenuEdit extends React.Component {
   }
   
   render() {
+    let typeArr = this.state.selNode.split('-')
+    let str = ""
+    let addType = 0
+    if (typeArr.length === 1 && this.state.selNode === "") {
+      str = "新增章"
+      addType = 0
+    }
+    else if (typeArr.length === 1 && this.state.selNode !== "") {
+      str = "在此节点下新增节"
+      addType = 1
+    }
+    else if (typeArr.length === 2) {
+      str = "在此节点下新增课"
+      addType = 2
+    }
+    else {
+      addType = null
+      str = "不可操作"
+    }
+    this.addType = addType
     return ([
-        <Tree onCheck={this.onCheck} checkStrictly={true} key={"tree"} loadData={this.onLoadData} checkable={true}
-              showLine>
-          {this.renderTreeNodes(this.state.chapterData)}
-        </Tree>,
+        <div key={"tree"}>
+          <h2>教材目录</h2>
+          <Tree onSelect={this.onSelect} checkStrictly={true} loadData={this.onLoadData}
+                showLine>
+            {this.renderTreeNodes(this.state.chapterData)}
+          </Tree>
+        </div>,
         <div key={"button"}>
           <Button
-            type={this.state.checkedKeys.length === 1 && this.state.checkedKeys[0].length < 5 ? "primary" : "dashed"}>新增</Button>
+            rel={addType}
+            type={this.state.selNode.length < 5 ? "primary" : "dashed"} onClick={this.add}>{str}
+          </Button>
           <Button type={"dashed"}>删除</Button>
-        </div>,<Modal key={"addvie8w"} title="Title"
-                              visible={false}
-                              onOk={this.ok}
-                              confirmLoading={confirmLoading}
-                              onCancel={this.handleCancel}
+        </div>,
+        <Modal
+          title="提示"
+          visible={this.state.showModal}
+          onOk={this.modalOK}
+          onCancel={this.modalCancel}
+          key={"modal"}
         >
-          <p>{ModalText}</p>
-        </Modal>,
+          {this.modalDom}
+        </Modal>
       ]
-    );
+    )
+      ;
   }
 }
 
@@ -262,7 +284,7 @@ class MenuConfigView extends React.Component {
     return (<div>
       <div className="title-bar"></div>
       <div className="menu-content">
-        <MenuEdit chapter={0} section={0} course={0}></MenuEdit>
+        <MenuEdit></MenuEdit>
       </div>
     </div>);
   }
